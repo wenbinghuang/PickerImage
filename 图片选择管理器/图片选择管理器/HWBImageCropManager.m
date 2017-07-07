e803d7f7ffc27db09e79f76a269381137c622d7d
wenbinghuang <952466986@qq.com>
2017-07-07 15:17:15 +0800

    完善裁减功能

--- 图片选择管理器/图片选择管理器/HWBImageCropManager.m
+++ 图片选择管理器/图片选择管理器/HWBImageCropManager.m
@@ -11,6 +11,106 @@
 
 @implementation HWBImageCropManager
 
++ (void)overlayClippingWithView:(UIView *)view cropRect:(CGRect)cropRect containerView:(UIView *)containerView needCircleCrop:(BOOL)needCircleCrop {
+    UIBezierPath *path = [UIBezierPath bezierPathWithRect:[UIScreen mainScreen].bounds];
+    CAShapeLayer *layer = [CAShapeLayer layer];
+    
+    if (needCircleCrop)
+        [path appendPath: [UIBezierPath bezierPathWithArcCenter:containerView.center radius:cropRect.size.width * 0.5 startAngle:0 endAngle:2 * M_PI clockwise:NO]];
+    else
+        [path appendPath: [UIBezierPath bezierPathWithRect:cropRect]];
+    
+    layer.path = path.CGPath;
+    layer.fillRule = kCAFillRuleEvenOdd;
+    layer.fillColor = [UIColor blackColor].CGColor;
+    layer.opacity = 0.5;
+    [view.layer addSublayer:layer];
+}
+
+
++ (UIImage *)cropImageView:(UIImageView *)imageView toRect:(CGRect)rect zoomScale:(double)zoomScale containerView:(UIView *)containerView {
+
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    CGRect imageViewRect = [imageView convertRect:imageView.bounds toView:containerView];
+    
+    CGPoint point = CGPointMake(imageViewRect.origin.x + imageViewRect.size.width / 2, imageViewRect.origin.y + imageViewRect.size.height / 2);
+    
+    CGFloat xMargin = containerView.frame.size.width - CGRectGetMaxX(rect) - rect.origin.x;
+    
+    CGPoint zeroPoint = CGPointMake((CGRectGetWidth(containerView.frame) - xMargin) / 2, containerView.center.y);
+    CGPoint translation = CGPointMake(point.x - zeroPoint.x, point.y - zeroPoint.y);
+    transform = CGAffineTransformTranslate(transform, translation.x, translation.y);
+    transform = CGAffineTransformScale(transform, zoomScale, zoomScale);
+    
+    CGImageRef imageRef = [self newTransformedImage:transform
+                                        sourceImage:imageView.image.CGImage
+                                         sourceSize:imageView.image.size
+                                        outputWidth:rect.size.width * [UIScreen mainScreen].scale
+                                           cropSize:rect.size
+                                      imageViewSize:imageView.frame.size];
+    UIImage *cropedImage = [UIImage imageWithCGImage:imageRef];
+    CGImageRelease(imageRef);
+    return cropedImage;
+}
+
++ (CGImageRef)newTransformedImage:(CGAffineTransform)transform sourceImage:(CGImageRef)sourceImage sourceSize:(CGSize)sourceSize  outputWidth:(CGFloat)outputWidth cropSize:(CGSize)cropSize imageViewSize:(CGSize)imageViewSize {
+    CGImageRef source = [self newScaledImage:sourceImage toSize:sourceSize];
+    
+    CGFloat aspect = cropSize.height/cropSize.width;
+    CGSize outputSize = CGSizeMake(outputWidth, outputWidth*aspect);
+    
+    CGContextRef context = CGBitmapContextCreate(NULL, outputSize.width, outputSize.height, CGImageGetBitsPerComponent(source), 0, CGImageGetColorSpace(source), CGImageGetBitmapInfo(source));
+    CGContextSetFillColorWithColor(context, [[UIColor clearColor] CGColor]);
+    CGContextFillRect(context, CGRectMake(0, 0, outputSize.width, outputSize.height));
+    
+    CGAffineTransform uiCoords = CGAffineTransformMakeScale(outputSize.width / cropSize.width, outputSize.height / cropSize.height);
+    uiCoords = CGAffineTransformTranslate(uiCoords, cropSize.width/2.0, cropSize.height / 2.0);
+    uiCoords = CGAffineTransformScale(uiCoords, 1.0, -1.0);
+    CGContextConcatCTM(context, uiCoords);
+    
+    CGContextConcatCTM(context, transform);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    
+    CGContextDrawImage(context, CGRectMake(-imageViewSize.width/2, -imageViewSize.height/2.0, imageViewSize.width, imageViewSize.height), source);
+    CGImageRef resultRef = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    CGImageRelease(source);
+    return resultRef;
+}
+
++ (CGImageRef)newScaledImage:(CGImageRef)source toSize:(CGSize)size {
+    CGSize srcSize = size;
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(NULL, size.width, size.height, 8, 0, rgbColorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(rgbColorSpace);
+    
+    CGContextSetInterpolationQuality(context, kCGInterpolationNone);
+    CGContextTranslateCTM(context, size.width/2, size.height/2);
+    
+    CGContextDrawImage(context, CGRectMake(-srcSize.width/2, -srcSize.height/2, srcSize.width, srcSize.height), source);
+    
+    CGImageRef resultRef = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    return resultRef;
+}
+
+/// 获取圆形图片
++ (UIImage *)circularClipImage:(UIImage *)image {
+    UIGraphicsBeginImageContextWithOptions(image.size, NO, [UIScreen mainScreen].scale);
+    
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGRect rect = CGRectMake(0, 0, image.size.width, image.size.height);
+    CGContextAddEllipseInRect(ctx, rect);
+    CGContextClip(ctx);
+    
+    [image drawInRect:rect];
+    UIImage *circleImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    return circleImage;
+}
+
 @end
 
 
@@ -27,9 +127,28 @@
         animatedImage = [UIImage imageWithData:data];
     else {
         
+        NSMutableArray *images = [NSMutableArray array];
+        NSTimeInterval duration = 0.0f;
+        
+        for (size_t i = 0; i < count; i++) {
+            
+            CGImageRef imageRef = CGImageSourceCreateImageAtIndex(source, i, NULL);
+            if (!imageRef)
+                continue;
+            duration += [self frameDurationAtindex:i source:source];
+            
+            [images addObject: [UIImage imageWithCGImage:imageRef scale:[UIScreen mainScreen].scale orientation:UIImageOrientationUp]];
+            
+            CGImageRelease(imageRef);
+        }
+        
+        if (!duration)
+            duration = (1.0f / 10.0f) * count;
+        
+        animatedImage = [UIImage animatedImageWithImages:images duration:duration];
     }
     
-    
+    CFRelease(source);
     
     return animatedImage;
 }
@@ -48,6 +167,7 @@
         NSNumber *delayTimeProp = gifProperties[(NSString *)kCGImagePropertyGIFDelayTime];
         if (delayTimeProp) {
             frameDuration = [delayTimeProp floatValue];
+            
         }
     }
     if (frameDuration < 0.011f) {
@@ -57,5 +177,7 @@
     CFRelease(cfFrameProperties);
     return frameDuration;
 }
+
+
 
 @end
